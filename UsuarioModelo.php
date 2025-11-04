daa3394a989ca24cc7f1221a04319e9e08ee2b73
FedericoGalcius <107211716+FedericoGalcius@users.noreply.github.com>
2025-11-04 00:21:44 -0300

    Archivos base

---
+++ UsuarioModelo.php
@@ -0,0 +1,54 @@
+<?php
+
+class UsuarioModelo{
+    public $conexion;
+
+
+function __construct  (){
+    $this->conexion=new mysqli("localhost","root","myReallyStrongPwd","mvc");
+
+}
+
+public function inserta($email, $nombre, $cedula, $password ){
+    $sql="insert into usuarios(email, nombre, cedula, passwords)
+    values('$email', '$nombre', '$cedula', '$password')";
+
+;
+   $this->conexion->query($sql);
+}
+
+public function login ($email, $password){
+
+    $sql="select admin from usuarios where email='$email' and passwords='$password' and socio=1 and admin=1;";
+   
+     //$this->conexion->query($sql);
+
+     
+         if ($this->conexion->query($sql)->num_rows >0){
+            return "admin";
+         }
+         else {
+            $sql="select * from usuarios where email='$email' and passwords='$password' and socio =1";
+            if ($this->conexion->query($sql)-num_rows >0){
+                return "socio";
+            }
+            else {
+                return "denegado";
+            }
+            
+         }
+             
+
+     return 1;
+}
+
+public function listarNoSocios (){
+    
+    $sql="select * from usuarios where socio=0;";
+    return $this->conexion->query($sql);
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+}
+
+?>
---
+++ controlador.php
@@ -0,0 +1,44 @@
+<?php 
+
+require "UsuarioModelo.php";
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$input = file_get_contents("php://input");
+$data = json_decode($input, true);
+
+
+$modelo = new UsuarioModelo();
+
+if ($data['accion']=="login"){
+
+$resultado = $modelo -> login($data['email'],$data['password']);
+
+if ($resultado=="admin") {
+    $r = $modelo->listarNoSocios();
+    header('Content-Type: application/json');
+    echo json_encode($r);
+    exit;
+} 
+else {
+    if ($resultado="socio"){
+        header('Location: ccop.html');
+    }
+    else{
+        header('Location: denegado.html');
+    }
+    exit;
+}
+}
+if ($data['accion']=="registrar"){
+$modelo -> inserta($data['email'],$data['nombre'],$data['cedula'],$data['password']);
+}
+
+
+
+
+?>
+
